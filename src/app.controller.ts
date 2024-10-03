c7b919826a2882a4958f493045890ac2444b6356
Wagner Reis <wagnerreis1997@gmail.com>
2024-10-02 22:00:44 -0300

    feat: send email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
+
+type NotificationPayload = {
+  email: string;
+  startAt: string;
+  endAt: string;
+  name: string;
+  title: string;
+  description: string;
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private mailerService: MailerService) {}
 
   @EventPattern('task_notification')
-  taskNotification(data: any) {
+  async taskNotification(data: NotificationPayload) {
     console.log('Received task notification:', data);
+    const result = await this.mailerService.sendMail({
+      to: data.email,
+      subject: 'Task Notification',
+      html: `
+        <body>
+          <h1> Olá ${data.name}</h1>
+
+          <span>Voce tem uma tarefa para hoje</span>
+          </br>
+          <span>Titulo: ${data.title}</span>
+          </br>
+          <span>Descrição: ${data.description}</span>
+          </br>
+          <span>Inicia em: ${data.startAt}</span>
+          </br>
+          <span>Termina em: ${data.endAt}</span>
+        </body>
+      `,
+      from: 'taskManager@nestjscourse.com.br',
+    });
+    console.log(result);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'aric.hickle@ethereal.email',
+          pass: 'fhRFfE56kjBWTwszKh',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>',
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
